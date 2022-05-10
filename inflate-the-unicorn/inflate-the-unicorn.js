f15afabc09e89963b22eb48ed912b0f9db04534e
Andrea Faundez <afaundezd@gmail.com>
2022-05-10 18:02:20 +1200

    unicorns have been inflated, finally

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,57 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+// -------------------- A few things to add to the html file -----------------------
+//onclick method: after googling A LOT, I saw that most people use the onclick method in the html file.
+//1.- add onclik to the images in the html file, calling the function that will change the images.
+
+//The second thing I notice is that most people use an id to apply the function to a certain element of the html file.
+//2.- give each image in the html file an id.
+//3.- use the id as the parameter of the function being called with onclick in each image.
+
+// ------------------------------------- Java ---------------------------------------
+//keep track of the unicorns.
+//4.- Write an object that has the id in the html file as its properties. Their inital value is the number each unicor is currently at.
+//    All of them star at 0 (0 clicks)
+
+//Accesing the images. If the images are in an array I can refer to the array in my function.
+//5.- Store the images in an array so I can pass them to the function.
+
+//function that will be call onclick, that changes the images.
+//    - check at what numer is the clicked unicorn at
+//    - if it is at 3 (has 3 clicks alrady), the forth click will show an alert message
+//    - if it is at any other number, the function will add 1 number to the unicorn and then,
+//    - change the image: get element by id and change the sorce
+//    - new sorce will be the array[i] after looping the array:
+//      -> I didn't work! :( it displays the last image at the first click and the message at the forth click
+//    - [i] has to be the same number that the unicorn is currently at.
+//    - array[i] will be expressed as array[object[imageid]]
+
+//my object
+let unicorns = {
+  unicorn1: 0,
+  unicorn2: 0,
+  unicorn3: 0,
+}
+
+let imgArray = [
+  './images/unicorn-0.png',
+  './images/unicorn-1.png',
+  './images/unicorn-2.png',
+  './images/unicorn-3.png',
+]
+
+//function that changes the images.
+function inflate(unicornId) {
+  if (unicorns[unicornId] === 3) {
+    alert('THANK YOU FELLA!')
+  } else {
+    unicorns[unicornId] = unicorns[unicornId] + 1
+    console.log(unicornId + ' is at ballon level ' + unicorns[unicornId])
+    //for (i = 0; i < imgArray.length; i++)
+    //document.getElementById(unicornId).src = imgArray[i]
+    document.getElementById(unicornId).src = imgArray[unicorns[unicornId]]
+  }
+}
