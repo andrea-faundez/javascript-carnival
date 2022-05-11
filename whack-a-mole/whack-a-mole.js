fe605664b6d67fa08ab8fa367960db2971dd03e6
Andrea Faundez <afaundezd@gmail.com>
2022-05-11 13:17:03 +1200

    the mole disappears when clicked on

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -14,16 +14,28 @@ console.log(cells)
 // - add src to the image element and adjust its width
 // - get a random cell using Math.floor and Math.random like we did with the javascript-cafe
 // - put the mole image in the chosen cell (appendChild method?)
-// - make the mole disappear when the cell is clicked (shoould this be a different function called in this function)
-// - make another mole appear when a mole disappers.
+
+let img = document.createElement('img')
+img.src = 'mole.PNG'
+img.width = '70'
 
 function mole() {
-  let img = document.createElement('img')
-  img.src = 'mole.PNG'
-  img.width = '70'
   let cell = cells[Math.floor(Math.random() * cells.length)]
   cell = cell.appendChild(img)
   console.log(cell)
 }
 
 mole()
+
+// Meke the mole disappera:
+// - addEventListener for when the image is clicked,
+// - function to make the mole disappears when clicked on
+// - make another mole appear when a mole disappers.
+
+document.addEventListener('click', whackedMole)
+
+function whackedMole(e) {
+  if (e.target.tagName === 'IMG') {
+    e.target.remove()
+  }
+}
