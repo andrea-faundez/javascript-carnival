fc2bcdb47ece157d9696956fe08d666593673a6d
Andrea Faundez <afaundezd@gmail.com>
2022-05-15 11:35:57 +1200

    a few explanations of the solution

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -4,35 +4,34 @@
 
 console.log('Dress The Clown!')
 
-// changing the head
-let clothingIndex = 0
-let indexes = [0, 0, 0]
+let clothingIndex = 0 // keeps track of wich part of the clown we are changing
+let indexes = [0, 0, 0] // this array contains the former headIndex, bodyIndex and shoesIndex and keeps track of them
 
 const head = document.getElementById('head')
 const body = document.getElementById('body')
 const shoes = document.getElementById('shoes')
 
-let imgs = [head, body, shoes]
-let strings = ['head', 'body', 'shoes']
+let imgs = [head, body, shoes] // created to change the right image in the changeHorizaontal function
+let strings = ['head', 'body', 'shoes'] //created to call the right image in the changeHorizontal function
 
 function changeHorizontal(shift) {
-  let index = indexes[clothingIndex]
+  let index = indexes[clothingIndex] // [clothingIndex] with this trick we ensure that the function has the same number in all of its parts
   let image = imgs[clothingIndex]
   let str = strings[clothingIndex]
 
-  index += shift
+  index += shift // shift will be determined by the eventListener ketdown arrow that we press (-1 or 1)
 
   if (index < 0) index = 5
 
   if (index > 5) index = 0
 
-  indexes[clothingIndex] = index
+  indexes[clothingIndex] = index // makes the changes applicable to the original indexes and clothingIndex variables
 
   image.src = './images/' + str + index + '.png'
 }
 
 function changeVertical(shift) {
-  clothingIndex += shift
+  clothingIndex += shift // shift will be determined by the eventListener ketdown arrow that we press (-1 or 1)
   if (clothingIndex > 2) clothingIndex = 0
   if (clothingIndex < 0) clothingIndex = 2
 }
